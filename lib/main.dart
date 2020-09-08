d7d81a0d3e8e5b6f82d8b9f23ef0b354fcf2c050
Metee7 <61970909+Metee7@users.noreply.github.com>
2020-09-08 21:40:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Movie App',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TrialAPP Tutorial'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //...
+                print('Answer 3 Chosen!');
+              },
+            ),
+          ],
+        ),
       ),
-      home: HomeScreen(),
     );
   }
 }
