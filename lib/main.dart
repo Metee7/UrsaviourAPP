8e4875bd645389f93e91816dc5f8cabcec761301
Metee7 <61970909+Metee7@users.noreply.github.com>
2020-09-20 17:40:41 +0300

    Update Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -463,3 +463,15 @@ class SecondRoute extends StatelessWidget {
     );
   }
 }
+
+class MyAccountsPage extends StatelessWidget with NavigationStates {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "My Accounts",
+        style: TextStyle(fontWeight: FontWeight.w900, fontSize: 28),
+      ),
+    );
+  }
+}
